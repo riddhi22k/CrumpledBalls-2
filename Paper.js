952638f05014baa4155600a16232a98fd1ed975a
riddhi22k <84701379+riddhi22k@users.noreply.github.com>
2021-05-31 20:30:25 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,27 @@
+class Paper{
+    constructor(x,y,r){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+        this.x=x;
+        this.y=y;
+        this.r=r;
+        this.image=loadImage("paper.png")
+        this.body=Bodies.circle(this.x,this.y,this.r,options)
+        World.add(world,this.body)
+    }
+    display(){
+        var paperPos=this.body.position;
+        push();
+       translate(paperPos.x,paperPos.y);
+       stroke(0);
+       imageMode(CENTER);
+       image(this.image,0,0,85,85)
+      // ellipseMode(RADIUS)
+      //ellipse(0,0,this.r,this.r)
+        pop();
+    }
+}
